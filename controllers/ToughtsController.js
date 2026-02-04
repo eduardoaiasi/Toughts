7b7ff503c518e48ace2885415ab246183c115d4e
eduardo <eduardo.diniz.aiasi@gmail.com>
2026-02-04 15:50:23 -0300

    feat: implement search and order functionality on home page

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -3,7 +3,32 @@ const User = require('../models/User');
 
 module.exports = class ToughtsController {
   static async showToughts(req, res) {
-    res.render('toughts/home');
+
+    let search = ''; 
+
+    if(req.query.search){
+      search = req.query.search;
+    }
+
+    const toughtsData = await Tought.findAll({ 
+      include: User,
+      where: {
+        title: {
+          [require('sequelize').Op.like]: `%${search}%`
+        }
+      },
+      order: [['createdAt', 'DESC']]
+    });
+
+    const toughts = toughtsData.map((result) => result.get({ plain: true }));
+
+    let toughtsQty = toughts.length;
+
+    if(toughtsQty === 0){
+      toughtsQty = false;
+    }
+
+    res.render('toughts/home', { toughts, search, toughtsQty });
   }
 
   static async dashboard(req, res) {
